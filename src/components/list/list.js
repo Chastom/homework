6eebcf0b20a2888c0484b042af7f67974345a839
chastom <delakata@gmail.com>
2019-06-02 22:02:55 +0300

    added local storage state and some fixes

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -12,9 +12,13 @@ class List extends React.Component {
       account_id: '',
       access_token: ''
     };
-    const state = window.localStorage.getItem('req_token');
-    if (state) {
-      this.state.req_token = state;
+    // const state = window.localStorage.getItem('req_token');
+    // if (state) {
+    //   this.state.req_token = state;
+    // }
+    const test = JSON.parse(window.localStorage.getItem('saved_state'));
+    if (test) {
+      this.state = test;
     }
   }
 
@@ -25,15 +29,18 @@ class List extends React.Component {
     }));
   };
 
-  requestAuthenticated(token) {
+  requestAuthenticated(token, api_key) {
     this.setState(() => ({
       req_token: token
     }));
-    window.localStorage.setItem('req_token', token);
+    const state = {
+      req_token: token,
+      authv4: api_key
+    };
+    window.localStorage.setItem('saved_state', JSON.stringify(state));
   }
   getRequestToken = () => {
     var api_key = this.state.authv4;
-    console.log(api_key);
     var self = this;
     console.log('Button clicked');
     var http = require('https');
@@ -59,7 +66,7 @@ class List extends React.Component {
         console.log(body.toString());
         var data = JSON.parse(body);
         var token = data.request_token;
-        self.requestAuthenticated(token);
+        self.requestAuthenticated(token, api_key);
         window.location = `https://www.themoviedb.org/auth/access?request_token=${token}`;
       });
     });
@@ -69,13 +76,22 @@ class List extends React.Component {
   };
 
   setAccess(access_token, account_id) {
+    const { authv4, req_token } = this.state;
     this.setState(() => ({
       access_token: access_token,
       account_id: account_id
     }));
+    const state = {
+      req_token: req_token,
+      authv4: authv4,
+      account_id: account_id,
+      access_token: access_token
+    };
+    window.localStorage.setItem('saved_state', JSON.stringify(state));
   }
 
   getAccess(req_token) {
+    var api_key = this.state.authv4;
     var self = this;
     var http = require('https');
 
@@ -85,8 +101,7 @@ class List extends React.Component {
       port: null,
       path: '/4/auth/access_token',
       headers: {
-        authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTY4ZDkyN2FlODRmYmMwNGRhMjJjYWEzOWUyYmJiZCIsInN1YiI6IjVjZWZkMzlhMGUwYTI2NmE0ZGNhOGVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.raoO_qEH7gvKBKr0gmiKCL5fGrd7oytNMkKbQi288-o',
+        authorization: `Bearer ${api_key}`,
         'content-type': 'application/json;charset=utf-8'
       }
     };
@@ -118,7 +133,6 @@ class List extends React.Component {
 
   render() {
     const { authv4, req_token, access_token, account_id } = this.state;
-    //console.log(authv4);
     return (
       <div>
         <div className={styles.InputBar}>
@@ -129,10 +143,14 @@ class List extends React.Component {
             type="text"
           />
         </div>
-        <button onClick={this.getRequestToken}>Approve</button>
-        {req_token}
-        <button onClick={() => this.getAccess(req_token)}>Get access</button>
-        {account_id}
+        <div>
+          <button onClick={this.getRequestToken}>Approve</button>
+          {req_token}
+        </div>
+        <div>
+          <button onClick={() => this.getAccess(req_token)}>Get access</button>
+          {account_id}
+        </div>
       </div>
     );
   }
