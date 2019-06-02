d0c91b8888344a43f49afe03ffac8448ecfdd235
chastom <delakata@gmail.com>
2019-06-02 21:34:13 +0300

    added logic to get access info

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -7,12 +7,12 @@ class List extends React.Component {
     super(props);
 
     this.state = {
-      suggestions: [],
-      text: '',
-      req_token: ''
+      authv4: '',
+      req_token: '',
+      account_id: '',
+      access_token: ''
     };
     const state = window.localStorage.getItem('req_token');
-    console.log(state);
     if (state) {
       this.state.req_token = state;
     }
@@ -21,55 +21,9 @@ class List extends React.Component {
   onTextChanged = e => {
     const value = e.target.value;
     this.setState(() => ({
-      text: value
+      authv4: value
     }));
-    this.getMovies(value);
   };
-
-  renderSuggestions() {
-    const { suggestions } = this.state;
-    if (suggestions.length === 0) {
-      return null;
-    }
-    var movies = suggestions.slice(0, 8);
-    return (
-      <ul>
-        {movies.map(movie => (
-          <li onClick={() => this.suggestionSelected(movie.original_title)} key={movie.id}>
-            <h3>{movie.original_title}</h3>
-            <p>
-              {movie.vote_average} Rating, {movie.release_date.toString().slice(0, 4)}
-            </p>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
-  suggestionSelected(value) {
-    this.setState(() => ({
-      text: value,
-      suggestions: []
-    }));
-  }
-
-  getMovies(text) {
-    if (text.length > 2) {
-      fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query=${text}&page=1&include_adult=false`
-      )
-        .then(res => res.json())
-        .then(res =>
-          this.setState(() => ({
-            suggestions: res.results
-          }))
-        );
-    } else {
-      this.setState(() => ({
-        suggestions: []
-      }));
-    }
-  }
 
   requestAuthenticated(token) {
     this.setState(() => ({
@@ -78,6 +32,8 @@ class List extends React.Component {
     window.localStorage.setItem('req_token', token);
   }
   getRequestToken = () => {
+    var api_key = this.state.authv4;
+    console.log(api_key);
     var self = this;
     console.log('Button clicked');
     var http = require('https');
@@ -88,8 +44,7 @@ class List extends React.Component {
       port: null,
       path: '/4/auth/request_token',
       headers: {
-        authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTY4ZDkyN2FlODRmYmMwNGRhMjJjYWEzOWUyYmJiZCIsInN1YiI6IjVjZWZkMzlhMGUwYTI2NmE0ZGNhOGVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.raoO_qEH7gvKBKr0gmiKCL5fGrd7oytNMkKbQi288-o',
+        authorization: `Bearer ${api_key}`,
         'content-type': 'application/json;charset=utf-8'
       }
     };
@@ -113,21 +68,71 @@ class List extends React.Component {
     req.end();
   };
 
+  setAccess(access_token, account_id) {
+    this.setState(() => ({
+      access_token: access_token,
+      account_id: account_id
+    }));
+  }
+
+  getAccess(req_token) {
+    var self = this;
+    var http = require('https');
+
+    var options = {
+      method: 'POST',
+      hostname: 'api.themoviedb.org',
+      port: null,
+      path: '/4/auth/access_token',
+      headers: {
+        authorization:
+          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTY4ZDkyN2FlODRmYmMwNGRhMjJjYWEzOWUyYmJiZCIsInN1YiI6IjVjZWZkMzlhMGUwYTI2NmE0ZGNhOGVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.raoO_qEH7gvKBKr0gmiKCL5fGrd7oytNMkKbQi288-o',
+        'content-type': 'application/json;charset=utf-8'
+      }
+    };
+
+    var req = http.request(options, function(res) {
+      var chunks = [];
+
+      res.on('data', function(chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on('end', function() {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+        var data = JSON.parse(body);
+        var access_token = data.access_token;
+        var account_id = data.account_id;
+        self.setAccess(access_token, account_id);
+      });
+    });
+
+    req.write(
+      JSON.stringify({
+        request_token: `${req_token}`
+      })
+    );
+    req.end();
+  }
+
   render() {
-    const { text, req_token } = this.state;
+    const { authv4, req_token, access_token, account_id } = this.state;
+    //console.log(authv4);
     return (
       <div>
         <div className={styles.InputBar}>
           <input
-            value={text}
+            value={authv4}
             onChange={this.onTextChanged}
             placeholder="Enter API Read Access Token"
             type="text"
           />
         </div>
-        <div className={styles.AutoCompleteText}>{this.renderSuggestions()}</div>
-        <button onClick={this.getRequestToken}>Click me</button>
+        <button onClick={this.getRequestToken}>Approve</button>
         {req_token}
+        <button onClick={() => this.getAccess(req_token)}>Get access</button>
+        {account_id}
       </div>
     );
   }
